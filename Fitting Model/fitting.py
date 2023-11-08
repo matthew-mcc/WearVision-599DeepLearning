55c22c6abd983650c4fec120340cf3907f2a977f
matthew-mcc <77706531+matthew-mcc@users.noreply.github.com>
2023-11-08 12:06:50 -0700

    Initial Setup

---
+++ Fitting Model/fitting.py
@@ -0,0 +1,12 @@
+'''
+Model 2: Fitting Clothing to Human Models
+
+Architecture Selection: Look into recent GAN architectures that have been applied successfully to similar tasks in the fashion domain.
+
+Data Preparation: Align the clothing images from your datasets with the generated human models' poses and sizes.
+
+Model Training and Tuning: Train your model on a diverse set of images to ensure robustness, using PyTorch's extensive library for custom layers and loss functions.
+
+Post-processing: Apply techniques to refine the output, ensuring realistic and high-quality images.
+
+'''
---
+++ Generative Model/generation.py
@@ -0,0 +1,12 @@
+'''
+Model 1: Generating Human Models from Text Prompts
+
+Research and Model Selection: Start by examining state-of-the-art text-to-image generation models. You might consider transformer models that have been successful in similar tasks.
+
+Data Preprocessing: Use PyTorch's torchvision.transforms for image preprocessing to match your model's input requirements.
+
+Model Training: Utilize a framework like PyTorch Lightning or Hugging Face Transformers to streamline training with PyTorch.
+
+Evaluation and Iteration: Implement evaluation metrics relevant to image generation to guide iterative improvements.
+
+'''
